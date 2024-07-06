0159a2220ba775e89c107db3ef1a74294cd7ff2f
Md Ashraful alam <ashrafulalam10000@gmail.com>
2024-07-06 18:22:06 +0600

    updated server auth

--- src/Redux/api/RootApi.js
+++ src/Redux/api/RootApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const rootApi = createApi({
   reducerPath: "Api",
   baseQuery: fetchBaseQuery({
-    baseUrl: `https://carelink-server-a8k3.onrender.com/`,
+    baseUrl: `https://carelink-server-a8k3.onrender.com/api/v1/`,
     credentials: 'include',
   }),
   middleware: getDefaultMiddleware =>
