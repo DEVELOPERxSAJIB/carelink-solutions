d68855638198628a49cf39e115bff8fa0141472a
Md Ashraful alam <ashrafulalam10000@gmail.com>
2024-07-09 20:55:21 +0600

    header

--- src/Pages/Patient/CreatePatient.jsx
+++ src/Pages/Patient/CreatePatient.jsx
@@ -1,8 +1,10 @@
 
 import PatientProfile from './../../components/Patient/CreatePatient';
+import PageHeader from './../../components/FormElement/PageHeader';
 
 const CreatePatient = () => {
   return <div className="card">
+     <PageHeader title="Patient details" className="card-header fs-3"/>
     <div className="card-body">
     <PatientProfile/>
     </div>
